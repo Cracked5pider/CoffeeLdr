2dc7242ac77312ce30ec7727f8d24ae1b6c5e563
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2022-11-07 14:58:44 -0500

    Update FindListOfBOFsToTEST.ps1

--- Bin/FindListOfBOFsToTEST.ps1
+++ Bin/FindListOfBOFsToTEST.ps1
@@ -1,2 +1,6 @@
 param ([Parameter(Mandatory)]$TopSearchDir)
 Get-ChildItem -Path $TopSearchDir -Filter *.o -Recurse -File -Name  -Exclude *x86* | ForEach-Object { [System.IO.Path]::GetFullPath($_)} | Set-Content FileList.txt
+
+ForEach ($item in (Get-Content $pwd\FileList.txt)) {
+    Start-Process -FilePath "$pwd\CoffeeLdr.x64.exe" -Wait -NoNewWindow -WorkingDirectory $pwd -ArgumentList "go",$item
+}
